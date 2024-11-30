871cb7da4aaa572d2b777a832dbd467bfb5e5548
monator16 <lys34900@naver.com>
2024-11-30 20:16:29 +0900

    weighted negative sampling train과 그냥 train함수 추가

--- src/main.py
+++ src/main.py
@@ -21,22 +21,19 @@ transform = Compose([])
 
 # 데이터 경로 설정
 dataset_dir = "./data/mp3"
-sim_set_dir = './data/filtered_lyrics_with_sets.csv'
-
-target_column = 'Set Index'
 
 # 오디오 파일 경로 및 데이터셋 준비
 train_dataset, val_dataset = create_contrastive_datasets(dataset_dir, train_ratio=0.8)
 
 # ContrastiveDataset으로 변환
 sample_rate = 44100 # [1, sample_rate*30]: 30초로 구간 설정
-train_contrastive_dataset = ContrastiveDataset(train_dataset, sim_set_dir, target_column, input_shape=[1, sample_rate*30], transform=transform)
-val_contrastive_dataset = ContrastiveDataset(val_dataset, sim_set_dir, target_column, input_shape=[1, sample_rate*30], transform=transform)
+train_contrastive_dataset = ContrastiveDataset(train_dataset, input_shape=[1, sample_rate*30], transform=transform)
+val_contrastive_dataset = ContrastiveDataset(val_dataset, input_shape=[1, sample_rate*30], transform=transform)
 
 # DataLoader로 배치 생성
 train_loader = DataLoader(train_contrastive_dataset, batch_size=16, shuffle=True)
 val_loader = DataLoader(val_contrastive_dataset, batch_size=16, shuffle=False)
-# -> 한 배치의 구성 : clip_a, clip_b, file_id, target_value
+# -> 한 배치의 구성 : clip_a, clip_b, file_id
 
 
 # ------------
@@ -44,8 +41,12 @@ val_loader = DataLoader(val_contrastive_dataset, batch_size=16, shuffle=False)
 # ------------
 from models import ContrastiveModel
 from ast_encoder import ASTEncoder
-from loss import soft_info_nce_loss
+from loss import soft_info_nce_loss, info_nce_loss
 from loss_weight import generate_lyrics_embeddings, compute_similarity
+
+# ------------
+# model
+# ------------
 
 # 1. 모델과 옵티마이저 초기화
 ######## 여기 만들어야 함!!!!!!!!!!!!
@@ -59,42 +60,79 @@ from transformers import BertTokenizer, BertModel
 bert_model = BertModel.from_pretrained("bert-base-uncased").to(device)
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
+# 3. 학습 루프 - 가사에 따라 가중치 설정한 negative sampling
+def train_weighted_negative_sampling(train_loader, model, optimizer, bert_model,tokenizer,device,num_epochs,batch_size):
+    for epoch in range(num_epochs):
+        for batch in train_loader:
+            clip_a, clip_b, file_ids, target_value = batch  # 오디오와 file_ids 로드
+
+            # 1) 오디오 임베딩 생성 (오디오는 로스 계산에만 사용)
+            projected_a, projected_b = model(clip_a, clip_b)
+
+            audio_embeddings = torch.cat([projected_a, projected_b], dim=0)  # Combine along the batch dimension
+            print(audio_embeddings.shape)
+
+            # 2) 가사 임베딩 생성
+            lyrics_embeddings = generate_lyrics_embeddings(file_ids, bert_model, tokenizer, device)
+
+            # 3) 가사 임베딩들 간의 유사도 계산
+            sim_ij = compute_similarity(lyrics_embeddings.repeat(2, 1))        
+
+            # 4) 손실 계산
+            loss = soft_info_nce_loss(
+                features=audio_embeddings,
+                sim_ij=sim_ij,
+                batch_size=batch_size,
+                n_views=2,
+                temperature=0.07,
+                device=device
+            )
+            loss = loss.requires_grad_()
+
+            # 5) 역전파 및 최적화
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}")
+
+def train_pure_negative_sampling(train_loader,model,optimizer, bert_model,tokenizer, device, num_epochs,batch_size):
+    for epoch in range(num_epochs):
+        for batch in train_loader:
+            clip_a, clip_b, file_ids, target_value = batch  # 오디오와 file_ids 로드
+
+            # 1) 오디오 임베딩 생성 (오디오는 로스 계산에만 사용)
+            projected_a, projected_b = model(clip_a, clip_b)
+
+            audio_embeddings = torch.cat([projected_a, projected_b], dim=0)  # Combine along the batch dimension
+            print(audio_embeddings.shape)
+
+            # 2) 가사 임베딩 생성
+            lyrics_embeddings = generate_lyrics_embeddings(file_ids, bert_model, tokenizer, device)
+
+            # 3) 가사 임베딩들 간의 유사도 계산
+            sim_ij = compute_similarity(lyrics_embeddings.repeat(2, 1))        
+
+            # 4) 손실 계산
+            loss = info_nce_loss(
+                features=audio_embeddings,
+                batch_size=batch_size,
+                n_views=2,
+                temperature=0.07,
+                device=device
+            )
+            loss = loss.requires_grad_()
+
+            # 5) 역전파 및 최적화
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}")
+
+#4. 학습루프 = 가중치 없는 negative sampling
 # 3. 학습 루프
-num_epochs = #설정해야 함
-batch_size = #설정해야 함
-model.train()
-
-for epoch in range(num_epochs):
-    for batch in train_loader:
-        clip_a, clip_b, file_ids = batch  # 오디오와 file_ids 로드
-        
-        clip_a = clip_a.to(device)
-        clip_b = clip_b.to(device)
-        # 1) 오디오 임베딩 생성 (오디오는 로스 계산에만 사용)
-        # AST이후 projection까지 
-        clip_a_embeddings, clip_b_embeddings = model(clip_a, clip_b)
-
-        #위의 임베딩 audio_embeddigs로 합쳐야 함 
-
-        # 2) 가사 임베딩 생성
-        lyrics_embeddings = generate_lyrics_embeddings(file_ids, bert_model, tokenizer, device)
-
-        # 3) 가사 임베딩들 간의 유사도 계산
-        sim_ij = compute_similarity(lyrics_embeddings.repeat(2, 1))
-        
-        # 4) 손실 계산
-        loss = soft_info_nce_loss(
-            features=audio_embeddings,
-            sim_ij=sim_ij,
-            batch_size=batch_size,
-            n_views=2,
-            temperature=0.07,
-            device=device
-        )
-
-        # 5) 역전파 및 최적화
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}")
+num_epochs = 1
+batch_size = 2
+train_weighted_negative_sampling(train_loader, model, optimizer, bert_model,tokenizer,device,num_epochs,batch_size)
+train_pure_negative_sampling(train_loader,model,optimizer, bert_model,tokenizer, device, num_epochs,batch_size)
