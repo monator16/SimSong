ab175761d3c00357065a556fd09b1a6214d44026
monator16 <lys34900@naver.com>
2024-11-30 15:52:14 +0900

    ast encoder (pretrained_ASTModel), models.py dim 수정

--- src/models.py
+++ src/models.py
@@ -6,23 +6,24 @@ class CotrastiveModel(nn.Module):
         super().__init__()
         self.audio_encoder = encoder
         self.proection_head = nn.Sequential(
-            nn.Linear(), #Ast 출력 차원에 맞게 조정
+            nn.Linear(self.audio_encoder.output_dim, 256), #Ast 출력 차원에 맞게 조정
             nn.ReLU(),
-            nn.Linear(,projection_dim) #최종 projection 차원
+            nn.Linear(256,projection_dim) #최종 projection 차원
 
         )
     
     def forward(self,clip_a, clip_b, file_id ):
-        embedding_a = self.audio_encoder(clip_a)
-        embedding_b = self.audio_encoder(clip_b)
+        clip_a_embeddings, clip_b_embeddings = self.audio_encoder.preprocess(clip_a, clip_b)
         
         # Projection
-        projected_a = self.projection_head(embedding_a)
-        projected_b = self.projection_head(embedding_b)
+        projected_a = self.projection_head(clip_a_embeddings)
+        projected_b = self.projection_head(clip_b_embeddings)
+
+        #projection 합치기
 
         #file_id는 가사 데이터를 불러오는 데 활용
         lyrics_a = self.load_lyrics(file_id)
 
         return projected_a, projected_b, lyrics_a
     
-    def 
+    
